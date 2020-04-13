aff2efdbbe06170dfb471e171c3adca139167cbf
Ivana Nikodinoska <ini@objektkultur.de>
2020-04-13 02:07:48 +0200

    moved our directory and shop data into its own reducer and created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -1,4 +1,7 @@
-const SHOP_DATA = [{
+//internal state
+
+const SHOP_DATA = {
+    hats: {
         id: 1,
         title: 'Hats',
         routeName: 'hats',
@@ -58,7 +61,7 @@ const SHOP_DATA = [{
             }
         ]
     },
-    {
+    sneakers: {
         id: 2,
         title: 'Sneakers',
         routeName: 'sneakers',
@@ -112,7 +115,7 @@ const SHOP_DATA = [{
             }
         ]
     },
-    {
+    jackets: {
         id: 3,
         title: 'Jackets',
         routeName: 'jackets',
@@ -148,7 +151,7 @@ const SHOP_DATA = [{
             }
         ]
     },
-    {
+    womens: {
         id: 4,
         title: 'Womens',
         routeName: 'womens',
@@ -196,7 +199,7 @@ const SHOP_DATA = [{
             }
         ]
     },
-    {
+    mens: {
         id: 5,
         title: 'Mens',
         routeName: 'mens',
@@ -238,6 +241,7 @@ const SHOP_DATA = [{
             }
         ]
     }
-];
+}
+
 
 export default SHOP_DATA;
